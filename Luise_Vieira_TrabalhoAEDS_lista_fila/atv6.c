6ca62ff197fb6ac305f57b6a9ac78790389718f9
Luise Vieira Castro <luisevieiracastro1@gmail.com>
2026-01-17 15:00:32 -0300

    Update atv6.c

--- Luise_Vieira_TrabalhoAEDS_lista_fila/atv6.c
+++ Luise_Vieira_TrabalhoAEDS_lista_fila/atv6.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//6. Escreva um algoritmo para juntar duas filas em uma única fila, de forma que os valores desta fila sejam intercalados. 
+// 6. Escreva um algoritmo para juntar duas filas em uma única fila, de forma que os valores desta fila sejam intercalados.
 
-#define MAXTAM 5
+#define MAXTAM 10
 typedef int apontador;
 typedef struct
 {
@@ -11,68 +11,74 @@ typedef struct
     apontador inicio;
     apontador fim;
     int qtd;
-}TipoFila;
+} TipoFila;
 
 void FFvazia(TipoFila *F)
 {
-    F->inicio=0;
-    F->fim=F->inicio;
-    F->qtd=0;
+    F->inicio = 0;
+    F->fim = F->inicio;
+    F->qtd = 0;
 }
 
-void enfileirar(TipoFila *F)
+void enfileirar(TipoFila *F, int num)
 {
-    int num=0;
-    if(F->qtd==MAXTAM)
+    if (F->qtd == MAXTAM)
     {
         printf("fila cheia!\n");
     }
-    
-    for(int i=0;i<MAXTAM;i++)
-    {
-        printf("Digite o %d valor:",i+1);
-        scanf("%d",&num);
 
-        F->item[F->fim]=num;//item na posição final da fila recebe o novo valor pq fila é fifo, primeiro que entra primeiro que sai e so pode adicionar no final
-        F->fim=(F->fim+1)%MAXTAM; //isso faz com que a posição ande, e o mod é pra fzr com que volte para o inicio pq é uma fila circular
-        F->qtd++;
+    F->item[F->fim] = num;          // item na posição final da fila recebe o novo valor pq fila é fifo, primeiro que entra primeiro que sai e so pode adicionar no final
+    F->fim = (F->fim + 1) % MAXTAM; // isso faz com que a posição ande, e o mod é pra fzr com que volte para o inicio pq é uma fila circular
+    F->qtd++;
 
-    }
-    //primeiro verificar se a fila esta cheia
-   
+    // primeiro verificar se a fila esta cheia
 }
 
 void intercalarFilas(TipoFila *A, TipoFila *B, TipoFila *C)
 {
     int i;
-    int posA=A->inicio;//ponteiros auxiliares pois nao mexemos com o inicio da fila real
-    int posB=B->inicio;
+    int posA = A->inicio; // ponteiros auxiliares pois nao mexemos com o inicio da fila real
+    int posB = B->inicio;
 
-    C->inicio=0;
-    C->fim=0;
-    C->qtd=0;
+    C->inicio = 0;
+    C->fim = 0;
+    C->qtd = 0;
 
-    for(i=0;i<A->qtd || i<B->qtd;i++)
+    for (i = 0; i < A->qtd || i < B->qtd; i++)
     {
-        if(i<A->qtd)
+        if (i < A->qtd)
         {
-            C->item[C->fim]=A->item[posA];//C recebe o item de A na ultima posição que ele tem disponivel
-            C->fim=(C->fim+1)%MAXTAM;//pro indice de c caminha pro ultimo e o mod é por conta de ser uma lista circular
+            C->item[C->fim] = A->item[posA]; // C recebe o item de A na ultima posição que ele tem disponivel
+            C->fim = (C->fim + 1) % MAXTAM;  // pro indice de c caminha pro ultimo e o mod é por conta de ser uma lista circular
             C->qtd++;
-            posA=(posA+1)%MAXTAM; //para avançar leitura da fila
+            posA = (posA + 1) % MAXTAM; // para avançar leitura da fila
         }
-        if(i<B->qtd)
+        if (i < B->qtd)
         {
-            C->item[C->fim]=B->item[posB];//C recebe o item de B na ultima posição que ele tem disponivel
-            C->fim=(C->fim+1)%MAXTAM;//pro indice de c caminha pro ultimo e o mod é por conta de ser uma lista circular
+            C->item[C->fim] = B->item[posB]; // C recebe o item de B na ultima posição que ele tem disponivel
+            C->fim = (C->fim + 1) % MAXTAM;  // pro indice de c caminha pro ultimo e o mod é por conta de ser uma lista circular
             C->qtd++;
-            posB=(posB+1)%MAXTAM; 
+            posB = (posB + 1) % MAXTAM;
         }
-
     }
-
 }
 
+void imprimirfila(TipoFila *F)
+{
+    if (F->qtd == 0) // conferir se a fila ta vazia
+    {
+        printf("\nFila vazia!\n");
+        return;
+    }
+
+    int pos = F->inicio; // ponteiro para leitura
+
+    for (int i = 0; i < F->qtd; i++) // quantidade de elementos validos
+    {
+        printf("%d ", F->item[pos]);
+        pos = (pos + 1) % MAXTAM; // chega no final do vetor-> volta pro começo
+    }
+}
 
 int main()
 {
@@ -82,7 +88,29 @@ int main()
     FFvazia(&filaA);
     FFvazia(&filaB);
     FFvazia(&filaC);
-    enfileirar(&filaA);
-    enfileirar(&filaB);
-    intercalarFilas(&filaA,&filaB,&filaC);
+    int num;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Fila A\n");
+        printf("Digite o %d valor:", i + 1);
+        scanf("%d", &num); 
+        enfileirar(&filaA, num);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nFila B\n");
+        printf("Digite o %d valor:", i + 1);
+        scanf("%d", &num);
+        enfileirar(&filaB, num);
+    }
+
+    intercalarFilas(&filaA, &filaB, &filaC);
+    printf("Fila A:\n");
+    imprimirfila(&filaA);
+    printf("\nFila B:\n");
+    imprimirfila(&filaB);
+    printf("\nFila intercalada:\n");
+    imprimirfila(&filaC);
+    return 0;
 }
